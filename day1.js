58c10aec03eee1be66b6a369a6059d86b46819bf
CameronHCouch <cameronhcouch@gmail.com>
2023-12-08 14:48:11 -0500

    Day1, Part2

--- day1.js
+++ day1.js
@@ -1,5 +1,17 @@
 const fs = require('node:fs');
 
+const numberMap = {
+  one: 'one1one',
+  two: 'two2two',
+  three: 'three3three',
+  four: 'four4four',
+  five: 'five5five',
+  six: 'six6six',
+  seven: 'seven7seven',
+  eight: 'eight8eight',
+  nine: 'nine9nine'
+}
+
 try {
   const data = fs.readFileSync('day1input.txt', 'utf8');
   const splitData = data.toString().split(/\r?\n/);
@@ -14,8 +26,30 @@ try {
 
 function findNumbers(line) {
   let join = 0;
+  line = convertTextToDigits(line);
   line = line.replace(/\D/g, '');
-  join = line[0] + line.slice(-1);
   
+  join = line[0] + line.slice(-1);
   return parseInt(join);
 }
+
+// Day 1, Part 2: reading left to right, replace text with digit if text spells number
+// Edge case: overlapping numbers BOTH count 🤦‍♂️
+// EX: fiveight = 58
+function convertTextToDigits(line) {
+  let newLine = line;
+  Object.keys(numberMap).forEach( textNum => {
+    const reggie = new RegExp(textNum, 'g');
+    newLine = newLine.replace(reggie, numberMap[textNum]);
+  })
+  return newLine;
+}
+
+// Leftover from Day 1, Part 1
+// function findDigits(line) {
+//   let join = 0;
+//   line = line.replace(/\D/g, '');
+//   join = line[0] + line.slice(-1);
+  
+//   return parseInt(join);
+// }
